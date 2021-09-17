dde28186db02bdef55ec3a5268256f812e08a6ee
NgChiuKuen <90508699+NgChiuKuen@users.noreply.github.com>
2021-09-18 00:03:28 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var button1;
+
+
+function setup() {
+  createCanvas(400,400);
+  button1 = createSprite (100,100,50,40);
+
+}
+
+function draw() 
+{
+  background(30)
+  drawSprites()
+}
+
+
+function mousePressed(RIGHT_ARROWpressd) {
+   background(255,0,0)
+}
